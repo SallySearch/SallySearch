aa5986187b253e70ae7401960ae89e9f543010b6
SallySearch <gunnar.beckers74@gmail.com>
2026-07-22 23:31:32 +0200

    Update app.js
    Suchbutton aktiviert

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
 
+
+function startSearch() {
+
+    const searchTerm = searchInput.value.trim();
+
+    if (searchTerm !== "") {
+
+        alert("Sally sucht nach: " + searchTerm);
+
+    }
+
+}
+
+
+// Suche mit Enter starten
 searchInput.addEventListener("keydown", function(event) {
 
     if (event.key === "Enter") {
 
-        const searchTerm = searchInput.value.trim();
-
-        if (searchTerm !== "") {
-
-            alert("Sally sucht nach: " + searchTerm);
-
-        }
+        startSearch();
 
     }
+
+});
+
+
+// Suche mit Button starten
+searchButton.addEventListener("click", function() {
+
+    startSearch();
 
 });
