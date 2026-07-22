f6966620a4b4f52fa4da89bf6cc106db8c91d75a
SallySearch <gunnar.beckers74@gmail.com>
2026-07-22 23:14:06 +0200

    Update app.js
    Erste Suchreaktion eingebaut

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const searchInput = document.getElementById("searchInput");
 
+searchInput.addEventListener("keydown", function(event) {
+
+    if (event.key === "Enter") {
+
+        const searchTerm = searchInput.value.trim();
+
+        if (searchTerm !== "") {
+
+            alert("Sally sucht nach: " + searchTerm);
+
+        }
+
+    }
+
+});
