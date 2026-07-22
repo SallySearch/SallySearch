1190b2c8b44f1c7a4992d48cf187261888dbe456
SallySearch <gunnar.beckers74@gmail.com>
2026-07-22 23:37:57 +0200

    Update searches.js
    Erste Recherchelinks eingebaut

--- searches.js
+++ searches.js
@@ -1,1 +1,40 @@
+function openSearches(category, term) {
 
+    let urls = [];
+
+
+    if (category === "vinyl") {
+
+        urls = [
+            "https://www.discogs.com/search/?q=",
+            "https://www.amazon.de/s?k=",
+            "https://www.ebay.de/sch/i.html?_nkw=",
+            "https://www.ebay.de/sch/i.html?_sofindtype=so&_nkw="
+        ];
+
+    }
+
+
+    if (category === "lego_used" || category === "lego_new") {
+
+        urls = [
+            "https://www.ebay.de/sch/i.html?_nkw=",
+            "https://www.ebay.de/sch/i.html?_sop=16&_nkw=",
+            "https://www.bricklink.com/v2/search.page?q=",
+            "https://www.amazon.de/s?k=",
+            "https://www.brickset.com/search/?query="
+        ];
+
+    }
+
+
+    urls.forEach(function(url) {
+
+        window.open(
+            url + encodeURIComponent(term),
+            "_blank"
+        );
+
+    });
+
+}
